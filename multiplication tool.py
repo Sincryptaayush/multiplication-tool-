fde29194b3b5de510a38f0016cca46cbf8f8435c
Sincryptaayush <108784856+Sincryptaayush@users.noreply.github.com>
2022-08-07 17:29:32 +0530

    Add files via upload

---
+++ multiplication tool.py
@@ -0,0 +1,63 @@
+
+#scripted by sincryptcra5h
+
+#multiplier tool
+
+import time
+
+banner= '''    #       #    #     # #     #  #####  #     # 
+   # #     # #    #   #  #     # #     # #     # 
+  #   #   #   #    # #   #     # #       #     # 
+ #     # #     #    #    #     #  #####  ####### 
+ ####### #######    #    #     #       # #     # 
+ #     # #     #    #    #     # #     # #     # 
+ #     # #     #    #     #####   #####  #     # 
+                                                 '''
+print(banner)
+                                                 
+                                                 
+print('\n')
+                                               
+
+
+aayush=input(' enter your name dear ! ------> ')
+
+print('\n')
+
+
+
+print('\n')
+
+
+print('\n')
+
+aayush_2=input(' please enter the password -->>>>>>>>>>> ' )
+print('\n')
+
+
+if aayush_2=='sincryption':
+   print(' your password is correct ' )
+
+else:
+  print(' your password is incorrect ' )
+  print('\n')
+  exit()
+print('\n')
+
+print('\n')
+
+
+maths1=input(f' Dear {aayush} enter your number 1 : ')
+print('\n')
+maths2=input(f' Dear {aayush} enter your number 2 : ')
+print('\n')
+
+maths1=int(maths1)
+maths2=int(maths2)
+
+overall=(maths1*maths2)
+
+print(f" dear {aayush}, \n The answer of your multiplication is ",overall)		
+print ('\n')
+
+exit()
